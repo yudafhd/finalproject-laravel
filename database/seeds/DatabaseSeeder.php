aec25953f640b83fa42098d2e2741d2db8981eb7
yudafahrudin <ahmadyudafahrudin@gmail.com>
2020-07-01 18:15:55 +0700

    Add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,49 +15,12 @@ class DatabaseSeeder extends Seeder
             'name' => 'superadmin',
             'username' => 'superadmin',
             'email' => 'superadmin@gmail.com',
-            'password' => Hash::make('Rpkbulog'),
+            'password' => Hash::make('pinterlink739155'),
             'access_type' => 'superadmin',
         ]);
 
-        DB::table('users')->insert([
-            'name' => 'admin',
-            'username' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('Rpkbulog'),
-            'access_type' => 'admin',
-        ]);
-
         DB::table('roles')->insert([
             'name' => 'admin',
-            'guard_name' => 'web',
-        ]);
-        DB::table('roles')->insert([
-            'name' => 'rpk',
-            'guard_name' => 'web',
-        ]);
-        DB::table('roles')->insert([
-            'name' => 'umum',
-            'guard_name' => 'web',
-        ]);
-
-        DB::table('permissions')->insert([
-            'name' => 'rpk',
-            'guard_name' => 'web',
-        ]);
-        DB::table('permissions')->insert([
-            'name' => 'items',
-            'guard_name' => 'web',
-        ]);
-        DB::table('permissions')->insert([
-            'name' => 'pemesanan',
-            'guard_name' => 'web',
-        ]);
-        DB::table('permissions')->insert([
-            'name' => 'penerimaan',
-            'guard_name' => 'web',
-        ]);
-        DB::table('permissions')->insert([
-            'name' => 'settings',
             'guard_name' => 'web',
         ]);
     }
