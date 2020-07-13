0c7855e5e8e0e746f5b69348f1c67ac216ab07c6
yudafahrudin <ahmadyudafahrudin@gmail.com>
2020-07-13 22:47:30 +0700

    Disable temporary login and register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,7 +11,7 @@
             <form
                 class="form-horizontal form-material"
                 method="POST"
-                action="{{ route('home') }}"
+                action="{{ route('register') }}"
             >
                 @csrf
                 <div class="form-group m-t-40">
