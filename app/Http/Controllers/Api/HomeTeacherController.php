e195a8072c5da6a4ae4fd7ba9d852ce8927f15f3
yudafahrudin <yuda@vontis.id>
2020-11-21 15:38:28 +0700

    fix date format

--- app/Http/Controllers/Api/HomeTeacherController.php
+++ app/Http/Controllers/Api/HomeTeacherController.php
@@ -6,14 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Absent;
 use App\Schedule;
-use App\Http\Resources\HomeCollection;
 
 class HomeTeacherController extends Controller
 {
 
     public function index(Request $request)
     {
-        $absent_today = Absent::with(['user', 'schedule'])->where('date_absent', '=', $request->date_absent)->get();
+        $absent_today = Absent::with(['user', 'schedule'])->where('date_absent', date('Y-m-d', strtotime($request->date_absent)))->get();
         $schedule_today = Schedule::with(['kelas', 'subject'])->where('user_id', '=', auth()->user()->id)->get();
         return response(['data' => [
             'absents' => $absent_today,
