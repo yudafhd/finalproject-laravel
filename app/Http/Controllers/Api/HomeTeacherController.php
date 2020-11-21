81c5d3ee2ec1ee642fa697348e4b63dc4c7296b2
yudafahrudin <yuda@vontis.id>
2020-11-21 16:23:06 +0700

    fix date format

--- app/Http/Controllers/Api/HomeTeacherController.php
+++ app/Http/Controllers/Api/HomeTeacherController.php
@@ -18,7 +18,7 @@ class HomeTeacherController extends Controller
         $absent_today21 = Absent::with(['user', 'schedule'])->where('date_absent', '2020-11-21')->get();
         $absent_today22 = Absent::with(['user', 'schedule'])->where('date_absent', '2020-11-22')->get();
         $absent_today23 = Absent::with(['user', 'schedule'])->where('date_absent', '2020-11-23')->get();
-        $absent_todaystr = Absent::with(['user', 'schedule'])->where('date_absent', date('Y-m-d', strtotime('2020-11-21')))->get();
+        $absent_todaystr = Absent::with(['user', 'schedule'])->where('date_absent', date('Y-m-d', strtotime($request->date_absent)))->get();
         $schedule_today = Schedule::with(['kelas', 'subject'])->where('user_id', '=', auth()->user()->id)->get();
         return response(['data' => [
             // 'absents' => $absent_today,
@@ -27,6 +27,7 @@ class HomeTeacherController extends Controller
             'absent_today22' => $absent_today22,
             'absent_today23' => $absent_today23,
             'absent_todaystr' => $absent_todaystr,
+            'request-date_absent' => $request->date_absent,
             'schedules' => $schedule_today
         ]]);
     }
