fd2859656e69dda7fce17367541d5bc3c84905a9
yudafahrudin <yuda@vontis.id>
2020-12-15 09:21:51 +0700

    Sort by day

--- app/Http/Controllers/Api/HomeTeacherController.php
+++ app/Http/Controllers/Api/HomeTeacherController.php
@@ -12,11 +12,36 @@ class HomeTeacherController extends Controller
 
     public function index(Request $request)
     {
-        $absent_today = Absent::with(['user', 'schedule'])->where('date_absent', date('Y-m-d', strtotime($request->date_absent)))->get();
-        $schedule_today = Schedule::with(['kelas', 'subject'])->where('user_id', '=', auth()->user()->id)->get();
+        $absent_today = Absent::with(['user', 'schedule'])
+            ->where('date_absent', date('Y-m-d', strtotime($request->date_absent)))
+            ->get();
+
+        // $schedule_today = Schedule::with(['kelas', 'subject'])
+        //     ->where('user_id', '=', auth()->user()->id)
+        //     ->get();
+
+        $schedule_today = $this->orderByDay(Schedule::with(['kelas', 'subject'])
+            ->where('user_id', '=', auth()->user()->id)
+            ->get());
+
         return response(['data' => [
             'absents' => $absent_today,
             'schedules' => $schedule_today
         ]]);
+    }
+
+    public function orderByDay($data)
+    {
+        $result = [];
+        $days = ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'minggu'];
+
+        foreach ($days as $key => $day) {
+            foreach ($data as $val) {
+                if ($day == $val->day) {
+                    $result[$key] = $val;
+                }
+            }
+        }
+        return $result;
     }
 }
