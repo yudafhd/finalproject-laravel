9cab6e144cf71471be6b285faea0560cde4f7344
yudafahrudin <yuda@vontis.id>
2020-12-28 18:32:59 +0700

    Add new feature and design

--- resources/views/daftar-okp.blade.php
+++ resources/views/daftar-okp.blade.php
@@ -13,32 +13,28 @@
         @foreach ($okps as $okp )
         <div class="col-md-4">
             <div class="card">
-            <div style="height:200px; background:#f3f1f1;
+                <a style="font-weight: bold; color:#67757c" href="{{ Route('detailokp', $okp->id) }}">
+                    <div style="height:200px; background:#f3f1f1;
               background-position: center;
             background-size: 400px auto;
              background-image: url('{{ asset('storage/okp/photo/'.$okp->foto) }}');"></div>
-            <div class="card-header" style="background-color: white !important; min-height: 150px!important">
-                <h3>
-                   <a style="font-weight: bold; color:#67757c" href="{{ Route('detailokp', $okp->id) }}">{{$okp->nama}}</a>
-                </h3>
-                <!-- <span style="font-size:15px">
-                    Bidang :  
-                   </span>
-                    <span style="font-weight:bold; font-size:12px;color:#1b6f6c">
-                              
-                    </span> -->
+                    <div class="card-header" style="background-color: white !important; min-height: 150px!important">
+                        <h4>
+                            {{$okp->nama}}
+                </a>
+                </h4>
+                <i class="mdi mdi-pin"></i> {{$okp->alamat}}
+                <br />
+                visi : {{ $okp->visi }}
+                <br />
+                misi : {{ $okp->misi }}
             </div>
-                <!-- <div class="card-body">
-
-                   <br />
-                </div> -->
-                  <div class="card-footer" style="font-size:14px; background:#f3f1f1; padding:10px">
-                      <i class="mdi mdi-pin"></i> 
-                      {{$okp->bidang}}
-                    </div>
+            <div class="card-footer" style="font-size:14px; background:#f3f1f1; padding:10px">
+                bidang {{$okp->bidang}}
             </div>
         </div>
-        @endforeach
     </div>
+    @endforeach
+</div>
 </div>
 @endsection
