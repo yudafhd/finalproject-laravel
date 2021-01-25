f1710ae14209b16e9786ec6f3fd3fda206842eeb
yudafahrudin <yuda@vontis.id>
2021-01-26 05:26:04 +0700

    Add conditional day absent

--- app/Http/Controllers/Api/AbsenteeismParentController.php
+++ app/Http/Controllers/Api/AbsenteeismParentController.php
@@ -56,8 +56,7 @@ class AbsenteeismParentController extends Controller
         try {
             date_default_timezone_set('Asia/Jakarta');
             $user = auth()->user();
-            $day = $this->switchDayName(date('D', $request->date));
-            $date = date('Y-m-d', $request->date);
+
 
             $validator = Validator::make(
                 $request->all(),
@@ -81,53 +80,143 @@ class AbsenteeismParentController extends Controller
                 Storage::disk('public')->put($path, $imagesBatch);
             }
 
-            $userAbsentToday = [];
-            $userScheduleToday = [];
+            if (is_array(json_decode($request->date))) {
+                foreach (json_decode($request->date) as $key => $value) {
+                    $day = $this->switchDayName(date('D', $value));
+                    $date = date('Y-m-d', $value);
+                    $userAbsentToday = [];
+                    $userScheduleToday = [];
 
-            $userAbsentToday = Absent::where('date_absent', $date)
-                ->where('user_id', '=', $user->id)
-                ->get();
+                    $userAbsentToday = Absent::where('date_absent', $date)
+                        ->where('user_id', '=', $user->id)
+                        ->get();
 
-            if ($request->schedule) {
-                foreach (json_decode($request->schedule) as $index => $value) {
-                    if ($value->status) {
-                        $userScheduleToday[$index] = $value;
+                    $userScheduleToday = Schedule::where('kelas_id', $user->kelas_id)
+                        ->where('day', $day)
+                        ->get();
+
+                    if (count($userAbsentToday) > 0 && count($userScheduleToday) > 0) {
+                        foreach ($userAbsentToday as $absent) {
+                            $absent->reason = $request->reasons;
+                            $absent->submit_from_teacher = 0;
+                            $absent->submit_from_admin = 0;
+                            $absent->submit_from_parent = 1;
+                            if ($request->description) {
+                                $absent->description = $request->description;
+                            }
+                            if ($imagename) {
+                                $absent->image = $imagename;
+                            }
+                            $absent->save();
+                        }
+                        $inArrayIds = [];
+                        if (count($userScheduleToday) > 0) {
+                            foreach ($userAbsentToday as $value) {
+                                $inArrayIds = array_merge($inArrayIds, [$value->schedule_id]);
+                            }
+                            foreach ($userScheduleToday as $usershcedule) {
+                                if (!in_array($usershcedule->id, $inArrayIds)) {
+                                    Absent::create([
+                                        'schedule_id' => $usershcedule->id,
+                                        'user_id' => $user->id,
+                                        'reason' => $request->reasons,
+                                        'date_absent' => $date,
+                                        'image' => $imagename,
+                                        'submit_from_parent' => 1,
+                                        'description' => $request->description
+                                    ]);
+                                }
+                            }
+                        }
+                    } else {
+                        if ($request->reasons !== 'masuk') {
+                            if (count($userScheduleToday) > 0) {
+                                foreach ($userScheduleToday as $schedule) {
+                                    Absent::create([
+                                        'schedule_id' => $schedule->id,
+                                        'user_id' => $user->id,
+                                        'reason' => $request->reasons,
+                                        'date_absent' => $date,
+                                        'image' => $imagename,
+                                        'submit_from_parent' => 1,
+                                        'description' => $request->description
+                                    ]);
+                                }
+                            } else {
+                                return response()->json([
+                                    'status' => 'error',
+                                    'message' => 'Tidak ada jadwal untuk mengajukan izin, pilih jadwal terlebih dahulu',
+                                ]);
+                            }
+                        }
                     }
                 }
             } else {
-                // $userScheduleToday = Schedule::where('day', $day)
-                //     ->where('kelas_id', $user->kelas_id)
-                //     ->get();
-            }
+                $day = $this->switchDayName(date('D', $request->date));
+                $date = date('Y-m-d', $request->date);
+                $userAbsentToday = [];
+                $userScheduleToday = [];
 
-            if (count($userAbsentToday) > 0 && count($userScheduleToday) > 0) {
-                if ($request->reasons == 'masuk') {
-                    foreach ($userAbsentToday as $absent) {
-                        $absent->delete();
+                $userAbsentToday = Absent::where('date_absent', $date)
+                    ->where('user_id', '=', $user->id)
+                    ->get();
+
+                if ($request->schedule) {
+                    foreach (json_decode($request->schedule) as $index => $value) {
+                        if ($value->status) {
+                            $userScheduleToday[$index] = $value;
+                        }
+                    }
+                }
+
+                $userScheduleToday = Schedule::where('kelas_id', $user->kelas_id)
+                    ->where('day', $day);
+
+                if (count($userAbsentToday) > 0 && count($userScheduleToday) > 0) {
+                    if ($request->reasons == 'masuk') {
+                        foreach ($userAbsentToday as $absent) {
+                            $absent->delete();
+                        }
+                    } else {
+                        foreach ($userAbsentToday as $absent) {
+                            $absent->reason = $request->reasons;
+                            $absent->submit_from_teacher = 0;
+                            $absent->submit_from_admin = 0;
+                            $absent->submit_from_parent = 1;
+                            if ($request->description) {
+                                $absent->description = $request->description;
+                            }
+                            if ($imagename) {
+                                $absent->image = $imagename;
+                            }
+                            $absent->save();
+                        }
+                        $inArrayIds = [];
+                        if (count($userScheduleToday) > 0) {
+                            foreach ($userAbsentToday as $value) {
+                                $inArrayIds = array_merge($inArrayIds, [$value->schedule_id]);
+                            }
+                            foreach ($userScheduleToday as $usershcedule) {
+                                if (!in_array($usershcedule->id, $inArrayIds)) {
+                                    Absent::create([
+                                        'schedule_id' => $usershcedule->id,
+                                        'user_id' => $user->id,
+                                        'reason' => $request->reasons,
+                                        'date_absent' => $date,
+                                        'image' => $imagename,
+                                        'submit_from_parent' => 1,
+                                        'description' => $request->description
+                                    ]);
+                                }
+                            }
+                        }
                     }
                 } else {
-                    foreach ($userAbsentToday as $absent) {
-                        $absent->reason = $request->reasons;
-                        $absent->submit_from_teacher = 0;
-                        $absent->submit_from_admin = 0;
-                        $absent->submit_from_parent = 1;
-                        if ($request->description) {
-                            $absent->description = $request->description;
-                        }
-                        if ($imagename) {
-                            $absent->image = $imagename;
-                        }
-                        $absent->save();
-                    }
-                    $inArrayIds = [];
-                    if (count($userScheduleToday) > 0) {
-                        foreach ($userAbsentToday as $value) {
-                            $inArrayIds = array_merge($inArrayIds, [$value->schedule_id]);
-                        }
-                        foreach ($userScheduleToday as $usershcedule) {
-                            if (!in_array($usershcedule->id, $inArrayIds)) {
+                    if ($request->reasons !== 'masuk') {
+                        if (count($userScheduleToday) > 0) {
+                            foreach ($userScheduleToday as $schedule) {
                                 Absent::create([
-                                    'schedule_id' => $usershcedule->id,
+                                    'schedule_id' => $schedule->id,
                                     'user_id' => $user->id,
                                     'reason' => $request->reasons,
                                     'date_absent' => $date,
@@ -136,38 +225,21 @@ class AbsenteeismParentController extends Controller
                                     'description' => $request->description
                                 ]);
                             }
-                        }
-                    }
-                }
-            } else {
-                if ($request->reasons !== 'masuk') {
-                    if (count($userScheduleToday) > 0) {
-                        foreach ($userScheduleToday as $schedule) {
-                            Absent::create([
-                                'schedule_id' => $schedule->id,
-                                'user_id' => $user->id,
-                                'reason' => $request->reasons,
-                                'date_absent' => $date,
-                                'image' => $imagename,
-                                'submit_from_parent' => 1,
-                                'description' => $request->description
+                        } else {
+                            return response()->json([
+                                'status' => 'error',
+                                'message' => 'Tidak ada jadwal untuk mengajukan izin, pilih jadwal terlebih dahulu',
                             ]);
                         }
-                    } else {
-                        return response()->json([
-                            'status' => 'error',
-                            'message' => 'Tidak ada jadwal untuk mengajukan izin, pilih jadwal terlebih dahulu',
-                        ]);
                     }
                 }
             }
+
             return response([
                 'status' => 'success',
                 'message' => 'absent berhasil diajukan',
                 'result' => count($userScheduleToday),
-                'date' => $date,
-                'inArrayIds' => $inArrayIds,
-                'userAbsentToday' => $userAbsentToday
+                'date' => json_decode($request->date)
             ]);
         } catch (\Exception $e) {
             return response(['status' => 'error', 'message' => $e->getMessage()]);
